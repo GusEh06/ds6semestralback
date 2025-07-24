85f67e2da912806570a364beeea7f515948afcf9
Omar Jaramillo <randolphjr27@gmail.com>
2025-07-24 15:39:32 -0500

    actualizacion y mejora de Registro-vista
    actualizacion de la partes de vregistro visitas

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    VisitanteViewSet, registro_usuario, obtener_usuario, mostrar_sendero, listar_senderos, mostrar_foto_sendero, listar_fotos_senderos
-,login_usuario
+    VisitanteViewSet, 
+    registro_usuario, obtener_usuario, 
+    mostrar_sendero, listar_senderos, 
+    mostrar_foto_sendero, listar_fotos_senderos,
+    login_usuario,
+    obtener_visitante_por_cedula, registrar_visita  # 🔹 NUEVOS IMPORTS
 )
+from api import views
 
 router = DefaultRouter()
 router.register(r'visitantes', VisitanteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    
+
     # Usuarios
     path('registro/', registro_usuario, name='registro-usuario'),
     path('usuario/<int:id>/', obtener_usuario, name='obtener-usuario'),
@@ -23,4 +28,12 @@ urlpatterns = [
     # Fotos de senderos
     path('foto-sendero/<int:id>/', mostrar_foto_sendero, name='mostrar-foto-sendero'),
     path('fotos-senderos/', listar_fotos_senderos, name='listar-fotos-senderos'),
+
+    # Visitantes
+    path('visitante/cedula/<str:cedula>/', obtener_visitante_por_cedula, name='visitante-por-cedula'),
+
+    # Registro de visita
+    path('registro-visita/', registrar_visita, name='registro-visita'),
+    path('registrar-visita-id/', views.registrar_visita_por_id, name='registrar-visita-id'),
+
 ]
