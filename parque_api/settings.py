b0e72f2a46914bfa5eb52feb124d159a0cb6ff66
GusEh06 <111920258+GusEh06@users.noreply.github.com>
2025-07-25 17:46:13 -0500

    Update settings.py

--- parque_api/settings.py
+++ parque_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')  # Lee la clave desde .env
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)  # Lee DEBUG desde .env
 
-ALLOWED_HOST = os.envieron.get("ALLOWED_HOST", "*").split(",")
+ALLOWED_HOST = os.environ.get("ALLOWED_HOST", "*").split(",")
 
 
 # Application definition
